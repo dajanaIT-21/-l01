7a754ccdc39cf77463d26dfa54f1e6dba7823013
dajanaIT-21 <98746095+dajanaIT-21@users.noreply.github.com>
2022-01-31 13:58:55 +0200

    Add files via upload

---
+++ harjutus02.grupp1.py
@@ -0,0 +1,15 @@
+# Harjutus 2
+# Dajana Mähdi
+# 31.01.2022
+
+#kolmnurga ümbermõõt
+a,b,c = 1,2,3
+p = a+b+c
+print("kolmnurga ümbermõõt on:",p)
+
+#toote hinna leidmine
+hind= 36.75
+ale= 0.4
+kogus= 3
+summa = (hind-(hind*ale))*3
+print("kolme toote summa:",summa,"€")
