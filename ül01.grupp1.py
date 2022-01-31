e028396d8972458176c3edd3d9ee76c5ad6c5544
dajanaIT-21 <98746095+dajanaIT-21@users.noreply.github.com>
2022-01-31 13:36:42 +0200

    Add files via upload

---
+++ ül01.grupp1.py
@@ -0,0 +1,28 @@
+# harjutus 01
+# Dajana Mähdi
+# IT-21
+# 31.01.2022
+'''
+a= int(input("sisesta üks külg a="))
+b= int(input("sisesta üks külg b="))
+S = a * b
+print("ristküliku pindala:",str(S)+"cm^2")
+'''
+
+'''
+print("tere mmaailm")
+print('tere sa ka')
+print('ütle nüüd: "tere maailm"')
+print(2222)
+print(22.3)
+'''
+
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+print("  @..@")
+print(" (----)")
+print("( \__/ )")
+print('^^ \"\" ^^')
+a = int(input("Sisesta ruudu külje pikkus: "))
+s = a * a
+print("vastus: ruudu pindala on ", str(s)+"cm^2")
+
