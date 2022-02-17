c19a0f1cf8a0e02cc12cc67ec720efee5f210bed
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2022-02-17 13:54:51 +0200

    Add files via upload

---
+++ ül8.g1.py
@@ -0,0 +1,36 @@
+####################
+#    ülesnne08     #
+#   dajana mähdi   #
+#    17.02.2022    #
+####################
+
+#autod
+
+class Auto:
+    automark='teadmata'
+    aasta=0
+    hind=0
+    
+    def kuvaMark(self):
+        print(self.automark)
+        
+    
+    def lisaMark(self,x):
+        self.automark=x
+        
+        
+minu=Auto()
+minu.lisaMark("Toyota")
+minu.kuvaMark()
+
+
+        
+        
+
+
+
+
+
+
+
+
