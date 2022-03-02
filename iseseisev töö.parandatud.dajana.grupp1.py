7dfff5192ade55557f6076a81bb76aae27ae78ba
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2022-03-02 16:25:41 +0200

    Update iseseisev töö.parandatud.dajana.grupp1.py

--- iseseisev töö.parandatud.dajana.grupp1.py
+++ iseseisev töö.parandatud.dajana.grupp1.py
@@ -1,18 +1,22 @@
-# Needed to create random numbers to simulate dice roll
+#TĆ¤ringud
+#kuvatakse korrektne arusaadav kĆ¼simus ja hiljem vastus - 1p
+#kasutaja vĆµistleb kahe tĆ¤ringuga arvuti vastu - 1p
+#kasutaja teeb pakkumise ning suurima tĆ¤ringupunktisumma vĆµitja saab laual oleva raha endale - 2p
+#kood kommenteeritud - 1p
+
 import random
 
-# Initialise player scores to 0
+#Täringu mäng
 Mängia1_score = 0
 pc_score = 0
 pank = 1000
 
-# Repeat everything in this block 10 times
+# Korda kõike selles blokkis 10 korda
 for i in range(10):
 
-    # Generate random numbers between 1 and 6 for each player.
+    # Looge iga mängija jaoks juhuslikud numbrid vahemikus 1 kuni 6.
     Mängia1_value = random.randint(1, 6)
     pc_value = random.randint(1, 6)
-
 
 def total_bank(pank):
     pank = 1000
@@ -21,25 +25,20 @@ while pank > 0:
     bet = int(input("Sisesta oma panus: "))
 
 
-
-
-
-    # Display the values
+    # Kuva väärtused
     Mängia1_pakkumine = random.randint(1, 101)
     print("Mängia 1 viskas: ", Mängia1_value)
     
     print("Arvuti viskas: ", pc_value)
     
    
-
- 
-    # Selection: based on comparison of the values, take the appropriate path through the code.
+    # väärtuste võrdluse põhjal vali sobiv tee läbi koodi
     if Mängia1_value > pc_value:
         print("Mängia 1 võidab.")
         print("Mängia 1 võidab kogu laual oleva raha:)")
         pank = pank + bet
         print(f"Sul on nüüd ${pank} pangakontol")
-        Mängia1_score = Mängia1_score +1  # This is how we increment a variable
+        Mängia1_score = Mängia1_score +1  # Nii suurendame muutujat
     elif pc_value > Mängia1_value:
         print("Arvuti võidab")
         print("Arvuti võidab kogu laual oleva raha:)")
@@ -50,7 +49,7 @@ while pank > 0:
     else:
         print("Sen viik")
 
-    input("Vajuta et jätkata.")  # Wait for user input to proceed.
+    input("Vajuta et jätkata.")  # Jätkamiseks oodake, kuni kasutaja sisestab.
 
 print("### Mäng läbi ###")
 print("Mängia 1 score:", Mängia1_score)
