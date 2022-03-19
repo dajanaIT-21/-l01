a86f72f685ba0b83577a23197a9677055ac4c942
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2022-03-19 22:14:07 +0200

    Add files via upload

---
+++ kilpkonn-ül3.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+#Akna seadistamine
+aken =Tk()
+aken.title('Tervitus')
+aken.configure(background='black')
+
+
+#Teksti lisamine
+Label(aken ,text='Nimi: Dajana Mähdi',foreground='red',background='black', font='Tahoma 16 bold italic',pady=10, padx=30).pack()
+Label(aken ,text='Rühm: IT21',foreground='red',background='black', font='Tahoma 16 bold italic',pady=10, padx=30).pack()
+Label(aken ,text='2021',foreground='red',background='black', font='Tahoma 16 bold italic',pady=10, padx=30).pack()
